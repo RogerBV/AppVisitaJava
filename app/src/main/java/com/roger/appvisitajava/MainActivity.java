30d7b5424a22949ddd4c55809300de1dcaba2b56
RogerBV <rogerbv_9017@hotmail.com>
2019-10-25 11:20:37 -0500

    Positions List

--- app/src/main/java/com/roger/appvisitajava/MainActivity.java
+++ app/src/main/java/com/roger/appvisitajava/MainActivity.java
@@ -8,7 +8,7 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    Button btnRegisterClient,btnListClients,btnPositionsList,btnEliminar,btnEnviar,btnActualizar;
+    Button btnRegisterClient,btnListClients,btnPositionsList,btnEliminar,btnSendVisits,btnActualizar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +19,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btnListClients.setOnClickListener(this);
         btnPositionsList = findViewById(R.id.btnPositionsList);
         btnPositionsList.setOnClickListener(this);
+        btnSendVisits = findViewById(R.id.btnSendVisits);
+        btnSendVisits.setOnClickListener(this);
     }
 
     @Override
@@ -27,7 +29,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         if(v.getId() == btnRegisterClient.getId())
         {
             i = new Intent(getApplicationContext(),RegisterVisitActivity.class);
-        }else{
+        }else if(v.getId() == btnListClients.getId()){
+            i = new Intent(getApplicationContext(),VisitsListActivity.class);
+        }else if(v.getId() == btnSendVisits.getId()){
+            i = new Intent(getApplicationContext(),SendVisitsActivity.class);
+        }else if(v.getId() == btnPositionsList.getId()){
+            i = new Intent(getApplicationContext(),MapsActivity.class);
+            i.putExtra("iTipo",2);
+        }
+        else{
             i = new Intent(getApplicationContext(),RegisterVisitActivity.class);
         }
         startActivity(i);
---
+++ app/src/main/java/com/roger/appvisitajava/PositionListActivity.java
@@ -0,0 +1,14 @@
+package com.roger.appvisitajava;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PositionListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_position_list);
+    }
+}
